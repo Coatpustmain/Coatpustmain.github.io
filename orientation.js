2aebc1cb800d7fc9e8c446bbace297a47e556766
Laurentiu Zidaru <coatpustmain@gmail.com>
2018-11-05 11:06:47 +0200

    Am adaugat motion

--- orientation.js
+++ orientation.js
@@ -1,6 +1,7 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.0";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.0";
 
 window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener("devicemotion", on_device_motion);
 
 //---------------------------------------------------------------------------
 function on_device_orientation(e)
@@ -21,3 +22,10 @@ function on_device_orientation(e)
 	context.stroke();
 }
 //---------------------------------------------------------------------------
+
+function on_device_motion(e)
+{
+	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity_z;
+	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity_x;
+	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity_y;
+}
