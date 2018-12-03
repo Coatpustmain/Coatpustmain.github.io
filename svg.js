1860cecec81d6e733fb831162269ccbe31deb820
Laurentiu Zidaru <coatpustmain@gmail.com>
2018-12-03 13:26:04 +0200

    AM ADAUGAT SVG v3

--- svg.js
+++ svg.js
@@ -11,6 +11,9 @@ function on_device_orientation(e)
 	
 	var R = 20;
 	
+	var svg_width = svg.getAttribute("width");
+	var svg_height = svg.getAttribute("height");
+	
 	circle.setAttribute("cx", svg_width / 2 + e.gamma / 90 * (svg_width / 2 - R) + "px");				
 	circle.setAttribute("cy", svg_height / 2 + e.beta / 90 * (svg_height / 2 - R) + "px");
 }
